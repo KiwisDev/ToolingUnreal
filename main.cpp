6ed4e3c0f9ea68d755e2028048ea23ea3f1bdbe7
Kiwis <69465940+KiwisDev@users.noreply.github.com>
2025-02-03 15:02:16 -0500

    Show infos + UBT

--- main.cpp
+++ main.cpp
@@ -8,21 +8,89 @@ using json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
     if (argc == 3) {
-        // Show-infos command
+        // -- Show-infos command
         if (strcmp(argv[2], "show-infos") == 0) {
-            clog << "[LOG] show-infos commands called" << endl;
+            json file = json::parse(ifstream(argv[1]));
+
+            // Name
+            cout << "Name : ";
+            try {
+                auto modules = file.at("Modules");
+                for (auto & module : modules) {
+                    if (module.at("Type") == "Runtime") {
+                        string name = module.at("Name");
+                        cout << name << endl;
+                    }
+                }
+            } catch (...) {
+                cout << "Not found" << endl;
+            }
+
+            // Version
+            cout << "Version : ";
+            try {
+                string version = to_string(file.at("EngineAssociation"));
+                version = version.substr(1, version.length()-2);
+                cout << ((version[0] == '{') ? "From source" : version) << endl;
+            } catch (...) {
+                cout << "Not found" << endl;
+            }
+
+            // Plugins
+            cout << "Plugins : ";
+            try {
+                auto plugins = file.at("Plugins");
+                for (auto & plugin : plugins) cout << endl << "\t" << plugin.at("Name");
+                cout << endl;
+            } catch (...) {
+                cout << "Not found" << endl;
+            }
+
+            return 0;
+        }
+
+        // -- Build project
+        if (strcmp(argv[2], "build") == 0) {
+            string command = R"(.\Engine\Build\BatchFiles\Build)";
+            #if defined(_WIN64)
+                command+= ".bat";
+            #elif defined(__linux__) || defined(__APPLE__)
+                command+= ".sh";
+            #else
+                cout << "OS not recognized" << endl;
+                return -1;
+            #endif
 
             json file = json::parse(ifstream(argv[1]));
 
-            return 0;
-        }
+            // Get name
+            string name;
+            try {
+                auto modules = file.at("Modules");
+                for (auto & module : modules) if (module.at("Type") == "Runtime") name = module.at("Name");
+            } catch (...) {
+                name = "Unknown";
+            }
+            if (name.empty()) { name = "Unknown"; }
 
-        // Build project
-        if (strcmp(argv[2], "build") == 0) {
+            // Configure command
+            command+= " " + name;
+            #if defined(_WIN64)
+                command+= " Win64 Development";
+            #elif defined(__linux__)
+                command+= " Linux Development";
+            #elif defined(__APPLE__)
+                command+= " Mac Development";
+            #endif
+            command+= ' ' + argv[1];
+            command+= " -waitmutex";
+
+            system(command.c_str());
+
             return 0;
         }
     } else if (argc == 4) {
-        // Package command
+        // -- Package command
         if (strcmp(argv[2], "package") == 0) {
             return 0;
         }
